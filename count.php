211288467b19dd268df749313fd0059d8f05d878
reytama86 <120538889+reytama86@users.noreply.github.com>
2023-12-20 16:17:49 +0700

    Update count.php

--- count.php
+++ count.php
@@ -1,16 +1,25 @@
 <?php
-$host = "localhost"; // Ganti dengan alamat host PostgreSQL Anda
-$port = 5432; // Port default PostgreSQL
-$username = "postgres"; // Ganti dengan nama pengguna PostgreSQL Anda
-$password = "123"; // Ganti dengan kata sandi PostgreSQL Anda
-$dbname = "vcount";
-$conn = pg_connect("host=localhost dbname=vcount user=postgres password=1234");
-$query = "SELECT * FROM visitor ORDER BY tanggal DESC";
-$ambilData = pg_query($conn, $query);
+$host = getenv('POSTGRES_HOST');
+$port = getenv('POSTGRES_PORT');
+$username = getenv('POSTGRES_USER');
+$password = getenv('POSTGRES_PASSWORD');
+$dbname = getenv('POSTGRES_DB');
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$username password=$password");
+
+if (!$conn) {
+    die("Koneksi gagal: " . pg_last_error());
+}
 
 $hariIni = date("Y-m-d");
 $kemarin = date("Y-m-d", strtotime("-1 day"));
 $totalAkhir = pg_fetch_assoc(pg_query($conn, "SELECT COUNT(*) FROM visitor"))['count'];
 $totalHariIni = pg_fetch_assoc(pg_query($conn, "SELECT COUNT(*) FROM visitor WHERE DATE(tanggal) = '$hariIni'"))['count'];
 $totalKemarin = pg_fetch_assoc(pg_query($conn, "SELECT COUNT(*) FROM visitor WHERE DATE(tanggal) = '$kemarin'"))['count'];
+
+$query = "SELECT * FROM visitor ORDER BY tanggal DESC";
+$ambilData = pg_query($conn, $query);
+
+// Tutup koneksi
+pg_close($conn);
 ?>
